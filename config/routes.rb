86ae82b3ef85ba092c2badb0cdd754d6c5b6b0ca
Elian Rojas <impresosgosen@gmail.com>
2021-03-29 11:06:20 -0600

    Models order and user created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admins
-  root 'home#index'
+  # root 'home#index'
+  #1 Autentificar solo para que los administradores modifiquen products y categories
+  authenticate :admin do
+    resources :products
+    resources :categories
+  #ACA TERMINA LA AUTENTIFICACION 1
+  end  
 end
