4f844d33794d4576a283eed947c1e9d6223dc905
Elian Rojas <impresosgosen@gmail.com>
2021-04-19 11:31:10 -0600

    Correciones a categorias

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :carts, only: %i[show update]
+  resource :carts, only: %i[show update]
   devise_for :users
   devise_for :admins
   root 'home#index'
+
+  
   #1 Autentificar solo para que los administradores modifiquen products y categories
   authenticate :admin do
     resources :products
